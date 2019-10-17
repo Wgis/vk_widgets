dcba97047bf335a3f37415313364add1969a2fde
Ilya Finogenov <bywgis@gmail.com>
2019-10-18 00:52:59 +0300

    Update vk_height.js

--- vk_height.js
+++ vk_height.js
@@ -1,65 +1,34 @@
-// инициализация вк
-VK.init(function() { 
-     console.log('Init successful 2');
-	$('#set-permission').on('click', function(e) {
-    		e.preventDefault();
-		SetPerms();
-	});
-	$('#set-widget').on('click', function(e) {
-		e.preventDefault();	
-		Install();
-	});
-  }, function() { 
-     console.log('Error init');
-}, '5.73'); 
-
-function SetPerms(){
-	console.log('Запрос прав доступа');
-	VK.callMethod("showGroupSettingsBox", 0);
-	 VK.addCallback('onGroupSettingsChanged', function(settings, grp_token){ 
-	   addServer(grp_token); 
-	 });
-
-	 VK.addCallback('onGroupSettingsCancel', function(){ 
-	   alert("Отказ в доступе"); 
-	 });	
-};
-
-function Install(){
-	console.log('Запрос установки виджета');
-
-	VK.callMethod('showAppWidgetPreviewBox', 'text', 'return {' + 
-	 '"title": "Цитата",' + 
-	 '"text": "Текст цитаты"' + 
-	'};');
-
-	VK.addCallback('onAppWidgetPreviewSuccess', function f(data){ 
-	 alert("Виджет успешно добавлен"); 
-	});
-};
-// функция дня изменения размера окна в зависимости от содержимого страницы
-function autosize(width) {
-    //Проверяем элемент body на наличие.
-    if (!document.getElementById('body')) {
-        alert('error');
-        return;
-    }
-    // Успешно ли подключен ВК скрипт
-    if (typeof VK.callMethod != 'undefined') {
-        /*
-        Вызываем функцию vk js api для управления окном.
-        VK.callMethod('функция', параметры)
-        В данном случае у нас - VK.callMethod('изменение_размеров_окна', ширина, высота);
-        Так же добавляем еще 60 пикселей что бы было небольшое расстояние.
-        */
-        VK.callMethod('resizeWindow', 840, document.getElementById('body').clientHeight + 60);
-    } else {
-        alert('error #2');
-    }
-}
-
-console.log('Перед jquery');
 $(document).ready( function(){
-    //Вызываем функцию регулировки высоты каждые пол секунды.
-    setInterval('autosize(607)', 500); 
+//Вызываем функцию регулировки высоты каждые пол секунды.
+setInterval('autosize(607)', 500); 
+
+$('#set-permission').on('click', function(e) {
+e.preventDefault();
+
+// запрос прав доступа для дальнейшего обновления данных посредством крона
+// после запрса создается ключ, который можно посмотреть на странице управления сообществом
+// его и нунжно будет применить для обновления данных в виджете через сервер
+// дока прав тут https://vk.com/dev/permissions
+// дока метода тут https://vk.com/dev/clientapi?f=3.+showGroupSettingsBox
+VK.callMethod("showGroupSettingsBox", 64);
+});
+
+$('#set-widget').on('click', function(e) {
+e.preventDefault();
+
+// запрос установки виджета
+// типы виджетов можно глянуть тут https://vk.com/dev/objects/appWidget
+// как подключить виджет можно глянуть тут https://vk.com/dev/apps_widgets
+VK.callMethod('showAppWidgetPreviewBox', 'text', 'return {' + 
+'"title": "Цитата",' + 
+'"text": "Текст цитаты"' + 
+'};');
+
+// типы событий, генерируемых после выполнения запроса на установку виджета можно глянуть тут https://vk.com/dev/apps_widgets
+// работа с событиями вк https://vk.com/dev/Javascript_SDK?f=4.1.+VK.addCallback
+VK.addCallback('onAppWidgetPreviewSuccess', function f(data){ 
+alert("Виджет успешно добавлен"); 
+});
+});	
+
 });
